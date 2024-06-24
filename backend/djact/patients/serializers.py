a8703d00c97c3150b373f366d1e17c73b52bd34a
YENG-SEBASTIAN <yengsebastian1@gmail.com>
2024-06-24 10:40:08 +0200

    Patient serializers

--- backend/djact/patients/serializers.py
+++ backend/djact/patients/serializers.py
@@ -1,9 +1,15 @@
-
-# patients/serializers.py
 from rest_framework import serializers
-from patients.models import PatientProfile, MedicalHistory, Allergy, Medication
+from patients.models import Registration, PatientProfile, MedicalHistory, Allergy, Medication, Vitals
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Registration
+        fields = '__all__'
+        read_only_fields = ('patient_id',)
 
 class PatientProfileSerializer(serializers.ModelSerializer):
+    patient = RegistrationSerializer()
+
     class Meta:
         model = PatientProfile
         fields = '__all__'
@@ -22,5 +28,8 @@ class MedicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Medication
         fields = '__all__'
-        
-        
+
+class VitalsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vitals
+        fields = '__all__'
