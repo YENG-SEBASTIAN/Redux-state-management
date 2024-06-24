dba2d7ce9c369bea31fc55dc69ab478ce611ae54
YENG-SEBASTIAN <yengsebastian1@gmail.com>
2024-06-24 10:59:41 +0200

    Patient views and urls

--- backend/djact/patients/urls.py
+++ backend/djact/patients/urls.py
@@ -1,14 +1,36 @@
-
 from django.urls import path
-from patients import views
+from patients.views import (
+    RegistrationListCreateAPIView, RegistrationDetailAPIView,
+    PatientProfileListCreateAPIView, PatientProfileDetailAPIView,
+    MedicalHistoryListCreateAPIView, MedicalHistoryDetailAPIView,
+    AllergyListCreateAPIView, AllergyDetailAPIView,
+    MedicationListCreateAPIView, MedicationDetailAPIView,
+    VitalsListCreateAPIView, VitalsDetailAPIView,
+)
 
 urlpatterns = [
-    path('profiles/', views.PatientProfileListCreateAPIView.as_view()),
-    path('profile/', views.PatientProfileRetrieveUpdateDestroyAPIView.as_view()),
-    path('medical-histories/', views.MedicalHistoryListCreateAPIView.as_view()),
-    path('medical-histories/', views.MedicalHistoryRetrieveUpdateDestroyAPIView.as_view()),
-    path('allergies/', views.AllergyListCreateAPIView.as_view()),
-    path('allergies/', views.AllergyRetrieveUpdateDestroyAPIView.as_view()),
-    path('medications/', views.MedicationListCreateAPIView.as_view()),
-    path('medications/', views.MedicationRetrieveUpdateDestroyAPIView.as_view()),
+    # Registration URLs
+    path('registrations/', RegistrationListCreateAPIView.as_view(), name='registration-list'),
+    path('registrations/<str:patient_id>/', RegistrationDetailAPIView.as_view(), name='registration-detail'),
+
+    # PatientProfile URLs
+    path('profiles/', PatientProfileListCreateAPIView.as_view(), name='patientprofile-list'),
+    path('profiles/<int:pk>/', PatientProfileDetailAPIView.as_view(), name='patientprofile-detail'),
+
+    # MedicalHistory URLs
+    path('medicalhistories/', MedicalHistoryListCreateAPIView.as_view(), name='medicalhistory-list'),
+    path('medicalhistories/<int:pk>/', MedicalHistoryDetailAPIView.as_view(), name='medicalhistory-detail'),
+
+    # Allergy URLs
+    path('allergies/', AllergyListCreateAPIView.as_view(), name='allergy-list'),
+    path('allergies/<int:pk>/', AllergyDetailAPIView.as_view(), name='allergy-detail'),
+
+    # Medication URLs
+    path('medications/', MedicationListCreateAPIView.as_view(), name='medication-list'),
+    path('medications/<int:pk>/', MedicationDetailAPIView.as_view(), name='medication-detail'),
+
+    # Vitals URLs
+    path('vitals/', VitalsListCreateAPIView.as_view(), name='vitals-list'),
+    path('vitals/<int:pk>/', VitalsDetailAPIView.as_view(), name='vitals-detail'),
 ]
+
